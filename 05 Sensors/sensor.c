ad152ba1f297e52d2a5c49ea3ea7e6dbfd4cc619
Rakesh Das <rakeshdaskowshik@gmail.com>
2015-11-09 08:43:10 -0600

    Adding sensors modules

---
+++ 05 Sensors/sensor.c
@@ -0,0 +1,7 @@
+// Created on Mon November 9 2015
+//This program will make use of the IR sensor and if statements to test if the robot is running on top of a black or white area
+int main()
+{
+	printf("Using sensors!\n");
+	return 0;
+}
